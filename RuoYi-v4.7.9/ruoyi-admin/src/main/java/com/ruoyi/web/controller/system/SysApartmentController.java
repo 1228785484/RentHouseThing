433002f11113863811c7161585163e9b1fd52f9e
a2004818 <1228785484@qq.com>
2024-09-25 08:55:58 +0800

    新增了一些提示

---
+++ RuoYi-v4.7.9/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysApartmentController.java
@@ -0,0 +1,12 @@
+package com.ruoyi.web.controller.system;
+
+import com.ruoyi.common.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//公寓控制器
+@Controller
+@RequestMapping("/system/apartments")
+public class SysApartmentController extends BaseController {
+
+}
