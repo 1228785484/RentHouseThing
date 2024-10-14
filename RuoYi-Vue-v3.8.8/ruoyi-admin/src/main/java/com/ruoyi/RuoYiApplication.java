703ee38901e8cb165a6d3ad7f1612aa83b918361
a2004818 <1228785484@qq.com>
2024-10-14 15:54:07 +0800

    内置了Redis

--- RuoYi-Vue-v3.8.8/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ RuoYi-Vue-v3.8.8/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * 启动程序
  * 
@@ -12,8 +15,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RuoYiApplication
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+        //D:\RentHouse\RuoYi-Vue-v3.8.8\ruoyi-admin\src\main\java\com\ruoyi\libs\Redis\redis-server.exe
+        ProcessBuilder processBuilder = new ProcessBuilder("RuoYi-Vue-v3.8.8\\ruoyi-admin\\src\\main\\java\\com\\ruoyi\\libs\\Redis\\redis-server.exe");
+        // 设置工作目录为项目的根目录
+        processBuilder.directory(new File(System.getProperty("user.dir")));
+        try {
+            processBuilder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
